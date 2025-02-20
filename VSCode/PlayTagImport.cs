a264c4b85df96d7265a992c9a50ced022459da38
ebe1kenobi <ebe1.kenob.i@gmail.com>
2025-02-20 15:46:06 +0100

    add importFile

---
+++ VSCode/PlayTagImport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TowerFall;
+using MonoMod.ModInterop;
+
+namespace TFModFortRiseGameModePlaytag
+{
+  [ModImportName("com.fortrise.TFModFortRiseGameModePlaytag")]
+  public static class PlayTagImport
+  {
+    public static Func<Modes, bool> IsGameModePlayTag;
+    public static Func<int, bool> IsPlayTagCountDownOn;
+    public static Func<int, bool> IsPlayerPlayTag;
+  }
+}
